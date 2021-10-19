8d90785b6452295e9624503b8b0d9cc7d280ebc9
scmwinlinaung <scm.winlinaung@gmaill.com>
2021-10-19 21:41:39 +0630

    changes

--- GenerateRSAKeyPair.js
+++ GenerateRSAKeyPair.js
@@ -11,55 +11,56 @@ function Encrypt ( base64Data, keyData )
 {
     const myEncrypter = new NodeRSA( keyData, "pkcs8-public-pem" );
     const cipherText = myEncrypter.encrypt( base64Data, 'base64' );
+
     return cipherText;
 }
 
 function Decrypt ( base64Data, keyData )
 {
     const myDecrypter = new NodeRSA( keyData );
-    // myDecrypter.setOptions( { encryptionScheme: 'pkcs1' } );
+    myDecrypter.setOptions( { encryptionScheme: 'pkcs1' } );
     const decipherText = myDecrypter.decrypt( base64Data, 'utf8' );
     console.log( decipherText );
     return decipherText;
 }
-
-// Encrypt( "WIN LIN HEHEHE", "-----BEGIN PUBLIC KEY-----"
-//     + "MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQEAqCHwqA1R/e+O6M5Pwh7Y"
-//     + "r7jepq5NdYZXw6ehsf3l5aJqdKSKKo5DtAp7r3gzB91xs9H/qNM+P4Z4iB9DgDGQ"
-//     + "hMX6MARRJiN56Pz2qWWA6zInJzwBW/ItsTmqY9bhRE7+xF/MmyLx/afJSV6O2UeV"
-//     + "2bkxWWNKTL+Wz/qLzb/XMlIGqMMastAXIamtQwDOVEzqviBj4P3JYSPnG8fZD0Mv"
-//     + "N4dVHBXhuB+iKpG+eOQjMX6qvudP94BmBp9Kx8U56tj8rMnrF+PPLWqiI3g77g/D"
-//     + "EyKTyjahM69kOKuye1Wm8k+PKGd3ldU9Esk9sa3Kx1/yF30fMNsmSxWs3rFwJZF0"
-//     + "/QICJxE="
-//     + "----- END PUBLIC KEY-----" );
-// Decrypt( cipherText,
+// Decrypt( "E8kGbXBEM06i5aFJmES2OCUOGY1UQxvdicu26pZxRMlUwpkESPRCfFvrcTcnLlyMS7j9YRlBHWSpwCZTScUtpx3cakTvZqLuLzmvtmH79vpaK7++AmfRAcutGZwRQaeAAy5DHIuExK/Pv9KqmyTsE00qNdIXY6KHBLygEoIP251V0PMZL3hJV+8ma4P+FerGpHljF1Mq4J5taqljBtKY/5wm3XGac2fS1qSnVV1o8ytrzKFo+qRZsyzJFfkoWp9hqUC2wvXf6HrEYNVObXQlM/mZwc9iyOyY3JmmeIR22wjmcgf0hZjXnoBF6DWPSfKbmkIkPaCf5mUQyL+LnvEqZQ==",
 //     "-----BEGIN PRIVATE KEY-----"
-//     + "MIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQCoIfCoDVH9747o"
-//     + "zk/CHtivuN6mrk11hlfDp6Gx/eXlomp0pIoqjkO0CnuveDMH3XGz0f+o0z4/hniI"
-//     + "H0OAMZCExfowBFEmI3no/PapZYDrMicnPAFb8i2xOapj1uFETv7EX8ybIvH9p8lJ"
-//     + "Xo7ZR5XZuTFZY0pMv5bP+ovNv9cyUgaowxqy0Bchqa1DAM5UTOq+IGPg/clhI+cb"
-//     + "x9kPQy83h1UcFeG4H6Iqkb545CMxfqq+50/3gGYGn0rHxTnq2PysyesX488taqIj"
-//     + "eDvuD8MTIpPKNqEzr2Q4q7J7VabyT48oZ3eV1T0SyT2xrcrHX/IXfR8w2yZLFaze"
-//     + "sXAlkXT9AgInEQKCAQAlL+zWnGCQu2w4tqEJogUTfRQ+XHXIB+eWtPw3IshjbNmT"
-//     + "AK8iN1+u5hWsgWJ/UhN3QJh8RPDeT6gv7EsihcdyyQH3HXHcqe32qT6EVLQNAndk"
-//     + "9OCi9yHi2md4z8REWdyOcczat3ssdMUUtQ092r53KbPvUeJoxjZlwGMc8hdHdia0"
-//     + "B1MU2vc6ZK3AVkV+MFR+sGlnQqZ9Na43zSRA8n7KEu+f4jtcJHZINrVHtIzj6kX0"
-//     + "Xtsa5oFV9nOvg1FUlk2c+orAovK1zbFmD8nU2co1Ijf9vLUX+G3tI1Cq+UbcDd1s"
-//     + "Soh0isxC66oaf3EM2O6Advzv2c5LCjQi2e8zSb5hAoGBANb/ZvkOLxqS6n4Xu5u5"
-//     + "rEjCzJgYSHp6596QHT9YA7O4BZC2V88u/+mc1jUeiVrQJj7B+PtfjkjYxcFXoQbW"
-//     + "VWlme7lCuLWKl70Vmgw9tnZOXaT+eThCk1Nmq/bbFu06z6Cy6QCckn6qGESC8lat"
-//     + "TgIBvIe+kNWi3hA3aHZr4OmfAoGBAMgyfppjJ3GkesS2vNQRvvH2jIf+/EbIbyzC"
-//     + "/cuPwPA/lmUeU3mn1Kzhn5256yoMpkjvixG9r5EQTwGio9bshtafd2vdi6T/00dF"
-//     + "3HlQfHOYIGJeiPgj3YrAvXXoiiSC35EKJZzW5tBCdac0nUimAH4KkNwJbJWeUVk1"
-//     + "OqJ/hJPjAoGAYOlIFvqk77FxqNNhsmTyGI4SwdTVLuOrz9svTghKXY1JB48JgOSG"
-//     + "S8M5+tBbMmFBIcqzCgWmFw7NGHY8p7mDCNT83o1shfJya87CtjD194jrhciVqHmL"
-//     + "sXvDY9RU38JaDojOMCSVEkNnoIhudsfO5UjxIBVKXzj0QsxPpZ+xr7kCgYBEYWsb"
-//     + "4NmjS/eo/eLCbtt4dCiGtinId3OcTiwlEnYRmzXAF0JsDjgbTz22WPKCa+NNQlVV"
-//     + "l0iwgxmuL533wT0vLEFC643R0JXG5LFjkZKCw3XhJS3WQO4PtCBBdhr2e5fZfEKa"
-//     + "EQmY0NhL84+/vDHTw52GnrKkgE77EbHjFVzeoQKBgF47O6ak/qu0vU13jlcpbntH"
-//     + "T5Mk3jQjQEzP+BsyF1FfbV9z45bf0KfFsO6+rl7qUgS3xNOSQZEkeV4IRW1B8MHn"
-//     + "oGgudOisDOPwUaL60ko+O4ItU/T3Z8axFLeEGlcrdMTYZIiVrD8GYPuZdG2e25i8"
-//     + "RmrFyoMqop9jnxRLY8aC"
+//     + "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCEoUK+zBOgBXbc"
+//     + "XmLLJw6NIToZMaoH1WV4ZLae//Po9oluyUQVY5sZL70wxYf0NyGMiU4d9FXg7S8e"
+//     + "Pl5ySrqgyB/p823JToJTKbfqYZH/8yl1H3AT8fIKhM2hu+130DWLRuo+gKS4xEYi"
+//     + "WxWk05KvLb3wVGTUz7H7GC1AGxDtTN+bgIE10yz3GD05ZvFzfZiGTRRjGP3fJ1C/"
+//     + "vIir4PJVghhczestHsGfzGv95LTg2yh1RZ6jgNhiP4hoOL7D84GRY0yBMAjgUiM6"
+//     + "GaDvtQiZasy9QORSscSi3f0xPSiGI50ScbQqgYJi14j++WOHPfWpEB0Uakn+jmQh"
+//     + "+3/AWcBrAgInEQKCAQAupCrzndnMe6m8NHEE6fMfRurPSowk5tsJuhwG0ZnQBJTt"
+//     + "nxBQhpOJxBKWb+fA4Raz1zTP/hlwelFBKH24/0Y2ZB779S4IAIBOPS5zhWZVReWm"
+//     + "Gn2m0Z2BxDeFg9JevKOBnFsHAWQV+OVXQuuosrzp+jM0uTVZ92IebN6jTdZ4J6hV"
+//     + "S6sE1Pr1mro6V5WCbj76ARfVM0T7G+9sjZKZCKZQQYbgR8fA6/zCYmfXEZwaWVga"
+//     + "HMIIep8FDkUHTWh/LVP/5vwZ5+1L5XZVJ+JkdImLwU4GJdixbd0HDGXEvH4loTVq"
+//     + "kYccEfBtOGHZ8NxvB0hi/UYE+cRRgUCwIC2N3k55AoGBAMZqM/DILzpOwmfSu5Eg"
+//     + "XiZIVxciy5X5jzPLIWo8v7tkYxXpAwnomXigG34kYigmSXFvbQr8MmBlTx3sKu2V"
+//     + "iXg93DCS2tRMjV8SPUB4kVAqPJGvER6npB7bpF/vd2pS3+gJ53BHRg+tnBgbuQmA"
+//     + "3Nl4+qDrABynYWdeKiRYO9LdAoGBAKsfYQMjMaQvS5iaesha82o3NVZp3gtkwxuK"
+//     + "bDBo1b0Sp380Qof68qyw8L1S7NNhKFzsEfkuiASqboatGADV4HqLopG6EPYsaHM5"
+//     + "7qMLBDmaunFroqsRy0Qc8eScMVhJiKZrJtEnUc6+DRl9VZBuXDItq50trn2FdlO9"
+//     + "JoPUxTfnAoGALqmaGhAbFGS9yDHDn2Dr7nv1dR9mYCQqLRlSk80Qf57J6+nneb4J"
+//     + "HPT+JX+cN2pMz84G2xYrOtbVZyfMdQA6IcF/75LIFQ3HmuBgURnfKP6lA2qfz+s4"
+//     + "VhjhNhJ73opQQGZC+L6epRrGo5sc5nY1A4Hx/T8f/13cZC409zfLGzECgYEAkUS+"
+//     + "8IPOPxk59FoU8B6FElDpa5Tf32KeaUqiIklhqlyg01kVO0zuke9Cx/yFrCXjS4ed"
+//     + "z1ydpwrIKLXbWHucVu585s80NNYDiD011fKfHeHh7ai0BSd//ZDaqES0A23bIur5"
+//     + "TUnZOMKdLe4zCFEx4Qe4d0ALYs0KEKrCrTGXN+0CgYEAubgwO48wGMdF+DWZ7aOW"
+//     + "W63BsuC7d6L6hpQoGCg8T66nrZ8wLqTsdrU+eUWMR5XgG8RL25XgHwgjxA6MnJD9"
+//     + "lliYW6Crc75rQY+NkU2sn+nXKK57h+s/jO8a2pyCJKN61reJnO6VNfzJa0vMFFho"
+//     + "nA/jqQ8AriKCwqapLfnWaeU="
 //     + "-----END PRIVATE KEY-----" )
+// Encrypt( "WIN LIN HEHEHE", "-----BEGIN PUBLIC KEY-----"
+//     + "MIIBITANBgkqhkiG9w0BAQEFAAOCAQ4AMIIBCQKCAQEAhKFCvswToAV23F5iyycO"
+//     + "jSE6GTGqB9VleGS2nv/z6PaJbslEFWObGS+9MMWH9DchjIlOHfRV4O0vHj5eckq6"
+//     + "oMgf6fNtyU6CUym36mGR//MpdR9wE/HyCoTNobvtd9A1i0bqPoCkuMRGIlsVpNOS"
+//     + "ry298FRk1M+x+xgtQBsQ7Uzfm4CBNdMs9xg9OWbxc32Yhk0UYxj93ydQv7yIq+Dy"
+//     + "VYIYXM3rLR7Bn8xr/eS04NsodUWeo4DYYj+IaDi+w/OBkWNMgTAI4FIjOhmg77UI"
+//     + "mWrMvUDkUrHEot39MT0ohiOdEnG0KoGCYteI/vljhz31qRAdFGpJ/o5kIft/wFnA"
+//     + "awICJxE="
+//     + "-----END PUBLIC KEY-----" );
+
 
 module.exports = { GenerateRSAKeyPair, Encrypt, Decrypt };
