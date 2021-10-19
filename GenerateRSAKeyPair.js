0023f9c11ea2e8982a2e887835349e59ba078523
scmwinlinaung <scm.winlinaung@gmaill.com>
2021-10-19 12:32:44 +0630

    second

--- GenerateRSAKeyPair.js
+++ GenerateRSAKeyPair.js
@@ -7,8 +7,11 @@ function GenerateRSAKeyPair ()
     return { public: keys.exportKey( "pkcs8-public-pem" ), private: keys.exportKey( "pkcs8-private-pem" ) }
 }
 
-
-
-GenerateRSAKeyPair();
-
+function Decrypt ( base64, privateKey )
+{
+    key.importKey( privateKey, "pkcs8-private-pem" )
+    const decipherText = key.decrypt( base64, "utf8" );
+    return decipherText;
+}
+// Decrypt( "niY8hB7HX7v0HvRUETa8dn35d677yfsyGKvTaG7H59fkLXHidQXCkIyjjZcixVyx0Rd7SgTlyJgxfOfsE2ENR2NtqcFjofPYKdUy64mKnNjLeKawPOcE5wEhImi1uw8yOz05GhAF9ExHTOsf1IcO8eEAk96Qfkler5yfbHRYBSK/hp2eeEdboj9bqEXKbeSxJWAyhEAW+3lPLI11OKvzfgPppjXNUL08OqraoFHDe96iid+8gQHM4/LgEnp+pJbE1GWsb2Lw9Q9pro9S+p4uOwUlbeBOLOiAYUaRctsPSKWrG8AlTJtdFuMv6EQF6moLQ5mpEXOyupMWt/3GaXOVvg==" )
 module.exports = { GenerateRSAKeyPair };
